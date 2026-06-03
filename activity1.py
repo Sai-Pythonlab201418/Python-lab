fca978bc44e1a3fa0daa9ea7d00a89498236ebf1
Sai-Pythonlab201418 <saiadwitiyachiluveru@gmail.com>
2026-06-03 18:16:03 +0530

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,9 +1,9 @@
-class Vehicle:
-    def __init__(self, name, max_speed, mileage):
-        self.name = name
-        self.max_speed = max_speed
-        self.mileage = mileage
-class Bus(Vehicle):
-    pass
-School_bus = Bus("School Volvo", 180, 12)
-print("Vehicle Name:", School_bus.name, "Speed:", School_bus.max_speed, "Mileage:", School_bus.mileage)
+class myClass:
+    __privateVar = 27;
+    def __privMeth(self):
+        print("I am inside class myClass")
+    def hello(self):
+        print("Private Variable Value :",myClass.__privateVar)
+foo = myClass ()
+foo.hello()
+foo.__privMeth
